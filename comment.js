84721c6b970940ea0d16c358e62496ef8032ebe1
May <92554133+qtmay@users.noreply.github.com>
2021-10-14 18:48:46 -0500

    Update comment.js

--- comment.js
+++ comment.js
@@ -30,8 +30,8 @@ var details = {
 	"accountName": "gaben@valvesoftware.com", //username
 	"password": "MoolyFTW", //password
     "steamguard": "", // only required if logging in with a Steam Guard authorization
-    "authCode": "XXXXX", // only required if logging in with a new email auth code
-    "twoFactorCode": "", // only required if logging in with a Steam Guard app code
+    "authCode": "", // only required if logging in with a new email auth code
+    "twoFactorCode": "XXXXX", // only required if logging in with a Steam Guard app code
     "captcha": "", // only required if you have been prompted with a CAPTCHA
     "disableMobile": false // pass `true` here to have node-steamcommunity not use the mobile login flow. This might help keep your login session alive longer, but you won't get an oAuth token in the login response.
 }
