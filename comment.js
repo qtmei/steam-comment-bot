caaf0c1b9abb97a560ec6b603ecc49f53640aae9
May <92554133+qtmei@users.noreply.github.com>
2025-01-03 18:39:31 -0600

    Update comment.js

--- comment.js
+++ comment.js
@@ -1,51 +1,42 @@
 const SteamCommunity = require('steamcommunity');
-
 let community = new SteamCommunity();
 
-function postcomment()
+var details = {
+	"accountName": "gaben@valvesoftware.com", //
+	"password": "MoolyFTW", //
+    "steamguard": process.argv[2], // only required if logging in with a Steam Guard authorization
+    "authCode": process.argv[3], // only required if logging in with a new email auth code
+    "twoFactorCode": "", // only required if logging in with a Steam Guard app code
+    "captcha": "", // only required if you have been prompted with a CAPTCHA
+    "disableMobile": false // pass `true` here to have node-steamcommunity not use the mobile login flow. This might help keep your login session alive longer, but you won't get an oAuth token in the login response.
+}
+
+function PostComment()
 {
-	community.postUserComment('76561197960287930', '<3', err => { //
+	community.postUserComment('76561199806777339', '<3', err => {
 		if(err)
 		{
-			console.log('posting failed');
 			console.log(err);
 			process.exit(1);
 		}
-		else
-		{
-			console.log('posting success');
-		}
 	});
+
+	console.log('posting success');
 }
 
-function post6comments()
+function Post6Comments()
 {
 	for(var i = 0; i < 6; i++)
-	{
-		setTimeout(postcomment, 2000);
-	}
-}
-
-var details = {
-	"accountName": "gaben@valvesoftware.com", //username
-	"password": "MoolyFTW", //password
-    "steamguard": "", // only required if logging in with a Steam Guard authorization
-    "authCode": "", // only required if logging in with a new email auth code
-    "twoFactorCode": "XXXXX", // only required if logging in with a Steam Guard app code
-    "captcha": "", // only required if you have been prompted with a CAPTCHA
-    "disableMobile": false // pass `true` here to have node-steamcommunity not use the mobile login flow. This might help keep your login session alive longer, but you won't get an oAuth token in the login response.
+		setTimeout(PostComment, 2000);
 }
 
 community.login(details, err => {
 	if(err)
 	{
-		console.log('login failed');
 		console.log(err)
-	}
-	else
-	{
-		console.log('login success');
-
-		setInterval(post6comments, 120000);
+		process.exit(1);
 	}
 });
+
+console.log('login success');
+setInterval(Post6Comments, 120000);
